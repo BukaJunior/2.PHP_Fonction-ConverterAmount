68d876939357ebc96d6725c8983ae6c8f3c1f0f7
BukaJunior <juniorbuka@gmail.com>
2023-10-23 00:00:39 +0200

    meme fonction mais en devise yens

--- index.php
+++ index.php
@@ -1,22 +1,40 @@
 <?php
 
-function getAmountInDollars(int $numberToConvert) {
+// function getAmountInDollars(int $numberToConvert) {
+
+//     // $amount = 42 ;
+//     // var_dump($amount);
+//     $numberToConvert = $_GET['number'];
+
+//     $tauxDeConversion = $numberToConvert * 1.14 ;
+//     // var_dump($tauxDeConversion);
+
+//     $amountInDollars = $tauxDeConversion ;
+//     // var_dump($amountInDollars);
+
+//     return $amountInDollars;
+// }
+
+// $dollars = getAmountInDollars(10);
+// echo $dollars;
+// echo getAmountInDollars(10);
+
+function getAmountInYens(int $numberToConvert) {
 
     // $amount = 42 ;
     // var_dump($amount);
     $numberToConvert = $_GET['number'];
 
-    $tauxDeConversion = $numberToConvert * 1.14 ;
+    $tauxDeConversion = $numberToConvert * 126 ;
     // var_dump($tauxDeConversion);
 
-    $amountInDollars = $tauxDeConversion ;
+    $amountInYens = $tauxDeConversion ;
     // var_dump($amountInDollars);
 
-    return $amountInDollars;
+    return $amountInYens;
 }
 
-$dollars = getAmountInDollars(5);
-echo $dollars;
-// echo getAmountInDollars(10);
+$yens = getAmountInYens(10);
+echo $yens;
 
 ?>
