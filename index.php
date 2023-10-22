cc340e97cbf737074a7da4869897033bd0ccdfff
BukaJunior <juniorbuka@gmail.com>
2023-10-22 23:29:55 +0200

    conversion d'un montant en dollard + création de la fonction sans déclaration

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$amount = 42 ;
+var_dump($amount);
+
+$tauxDeConversion = $amount * 1.14 ;
+var_dump($tauxDeConversion);
+
+$amountInDollars = $tauxDeConversion ;
+var_dump($amountInDollars);
+
+function getAmountInDollars() {
+
+
+}
+
+?>
