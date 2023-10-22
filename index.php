863e677cc2b8d3972362c939d0fefda2e69519c4
BukaJunior <juniorbuka@gmail.com>
2023-10-22 23:36:38 +0200

    execution de la fonction

--- index.php
+++ index.php
@@ -3,15 +3,18 @@
 function getAmountInDollars() {
 
     $amount = 42 ;
-    var_dump($amount);
+    // var_dump($amount);
 
     $tauxDeConversion = $amount * 1.14 ;
-    var_dump($tauxDeConversion);
+    // var_dump($tauxDeConversion);
 
     $amountInDollars = $tauxDeConversion ;
-    var_dump($amountInDollars);
+    // var_dump($amountInDollars);
 
     return $amountInDollars;
 }
+
+$dollars = getAmountInDollars();
+echo $dollars;
 
 ?>
